96623fd2ecc3038a71644dd7ce66b4be5ed458b8
LeinerViloria <vilorialeiner2002@gmail.com>
2022-03-06 16:00:50 -0500

    Se crean directorios, pero hay problemas con el snackBar

--- frontend/src/app/phonebook/create/create.component.ts
+++ frontend/src/app/phonebook/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PhonebookService } from 'src/app/services/phonebook.service';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-create',
@@ -7,8 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
   registerData:any;
+  message:string="";
+  horizontalPosition:MatSnackBarHorizontalPosition="right";
+  verticalPosition:MatSnackBarVerticalPosition="top";
+  durationInSeconds:number=2000;
 
-  constructor() {
+  // constructor(private _phoneBook:PhonebookService, private _router:Router, private _snackBar:MatSnackBar) {
+  //   this.registerData={};
+  // }
+
+  constructor(private _phoneBook:PhonebookService,  private _router:Router){
     this.registerData={};
   }
 
@@ -16,7 +27,41 @@ export class CreateComponent implements OnInit {
   }
 
   registerP_B(){
-    
+    if(!this.registerData.name){
+      this.message="Incomplete data"; 
+      this.openSnackBarError();
+    }else{
+      this._phoneBook.registerP_B(this.registerData).subscribe({
+        next: (u)=>{
+          this._router.navigate(['/']);
+          this.message="Phonebook created";
+          this.openSnackBarSuccesfull();
+        },
+        error:(e)=>{
+          this.message="Error to create this phonebook";
+          this.openSnackBarSuccesfull();
+        }
+      });
+    }
   }
+
+  openSnackBarSuccesfull() {
+    // this._snackBar.open(this.message, 'X', {
+    //   horizontalPosition: this.horizontalPosition,
+    //   verticalPosition: this.verticalPosition,
+    //   duration: this.durationInSeconds,
+    //   panelClass: ['styleSnackBarSuccesfull'],
+    // });
+    alert(this.message);
+  }
+  openSnackBarError() {
+    // this._snackBar.open(this.message, 'X', {
+    //   horizontalPosition: this.horizontalPosition,
+    //   verticalPosition: this.verticalPosition,
+    //   duration: this.durationInSeconds,
+    //   panelClass: ['styleSnackBarError'],
+    // });
+    alert(this.message);
+ }
 
 }
